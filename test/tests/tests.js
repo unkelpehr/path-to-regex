6202db04a585e038969157473183c40eee29d163
Pehr Boman <unkelpehr@gmail.com>
2015-11-26 21:50:40 +0100

    Added `length` property and updated tests

--- test/tests/tests.js
+++ test/tests/tests.js
@@ -11,3 +11,17 @@ testRunner(function (tests, testsDescr) {
 		assert.end();
 	});
 });
+
+tape('Miscellaneous', function (assert) {
+	var regex;
+
+	regex = assert.ok(typeof pathToRegexp('/users/:userId?').query === 'function', 'regex.query is a function');
+
+	regex = assert.equal(pathToRegexp('/users/:userId').length, 1, '`length` equals 1');
+	regex = assert.equal(pathToRegexp('/users/:userId/:userId?').length, 2, '`length` equals 2');
+	regex = assert.equal(pathToRegexp('/users/:userId/:userId?/*').length, 3, '`length` equals 3');
+	regex = assert.equal(pathToRegexp('/users/').length, 0, '`length` equals 0');
+	regex = assert.equal(pathToRegexp('*').length, 1, '`length` equals 1');
+
+	assert.end();
+});
